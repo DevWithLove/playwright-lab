feb17c6c5a5b9b10c9246e52a0ea0494bc5fe05f
Tony Mu <tony.mu@vacasa.com>
2026-01-31 13:08:54 +1000

    Updated readme

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -11,8 +11,12 @@ test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  // await page.getByRole('link', { name: 'Get started' }).click();
+
+  // OR: Create a locator.
+  const getStarted = page.getByRole('link', { name: 'Get started' });
+  await getStarted.click();
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+})
